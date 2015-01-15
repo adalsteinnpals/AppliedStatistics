051118a6e87ffcedc048e845f83d6cf6dfc04ee3
adalsteinnpals <adalsteinnpals@gmail.com>
2015-01-15 16:29:53 +0100

    as

--- naes.R
+++ naes.R
@@ -1,7 +1,5 @@
 dev.off()
 rm(list=ls())
-setwd("/Users/leifurbjarkierlendsson/Dropbox/Applied_Statistics_Shared/Leifur/Applied_statistics/Assignment2")
-#dir("/Users/leifurbjarkierlendsson/Dropbox/Applied_Statistics_Shared/Leifur/Applied_statistics/Assignment2")
 
 data.1 <- read.table("campy_pre2002.txt",header=T,sep="\t", fill = TRUE)
 data.2 <- read.table("campy_2002-2005.csv",header=T,sep=",")
@@ -11,9 +9,7 @@ summary(data.1)
 data.1 <- subset(data.1, (!SEKTION=="res" & AKTVNR==5133 & CHR_NR>=10000))
 data.2  <- subset(data.2, Chrnr>=10000)
 data.3  <- subset(data.3, Chrnr>=10000)
-jhasd
 
-Sys.setlocale(category = "LC_TIME","C")
 data.1$PRV_DATO  <- as.Date(data.1$PRV_DATO,format="%d %b %Y")
 data.2$Prvdato  <- as.Date(data.2$Prvdato,format="%m/%d/%y")
 data.3$Provedato  <- as.Date(data.3$Provedato,format="%m/%d/%y")
@@ -33,12 +29,18 @@ colnames(data.3) <- column.name
 data <- rbind(data.1, data.2,data.3)
 
 data <- subset(data,(!is.na(epinr) &chrnr>=10000))
+
+levels(data$resultat) 
+levels(data$resultat)  <- c("NEG","POS","POS","POS","POS","POS","POS","POS","POS","NEG","POS","NEG","POS")
+
 data <- subset(data,duplicated(jnr)!=TRUE)
 
 data  <- subset(data, matr =="Kloaksvaber"| matr =="Svaberprøve" | matr =="766" | matr =="772")
 
 data$weeknr  <- as.numeric( format(data$prvdato+3, "%U"))
 
-data  <- subset(data,weeknr%%2==0)
+data  <- subset(data,weeknr>=0)
 
 #remove chrnr if it is not repeated at least ten times
+
+test <- ddply(data,.(chrnr),nrow)
