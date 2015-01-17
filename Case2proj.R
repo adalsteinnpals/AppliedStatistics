ea375e8ee0b3762f7e22ff5e31079ce5a5edcccb
leiberl <einkabanki02>
2015-01-17 13:47:48 +0100

    asdfsaf

--- Case2proj.R
+++ Case2proj.R
@@ -3,14 +3,37 @@ rm(list=ls())
 
 data <- read.table("case2regionsOnePerBatch.txt",header=T)
 
-plot(data$pos[1:52]/data$total[1:52],typ="l")
+#visualization of the data
 data$ratio <- (data$pos/data$total)
 
+plot(data$ratio,typ="l")
+plot(data$ratio[1:52],typ="l")
 
-<<<<<<< HEAD
- 
-=======
-lm1 <- lm(ratio~.,data)
+names(data)
+data.primary <- data[,c("aveTemp","relHum","sunHours","precip","ratio")]
+
+#plot of the correlation between variables
+pairs(data.primary,panel=panel.smooth)
+
+#install.packages("tree")
+#library(tree)
+model1<-tree(ratio~.,data.primary)
+
+plot(model1)
+text(model1)
+#split the aveTemp in 11.25
+
+#install.packages("mgcv")
+library(mgcv)
+par(mfrow=c(2,2))
+model2<-gam(ratio~s(aveTemp)+s(relHum)+s(sunHours)+s(precip),data=data.primary)
+summary(model2)
+plot(model2)
+
+
+
+
+lm1 <- lm(ratio~.,data.primary)
+
 anova(lm1)
 lm2 <- step(lm1)
->>>>>>> 748d3903458dec7230e469618f3679541c4a1eb3
