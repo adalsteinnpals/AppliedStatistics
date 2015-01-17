748d3903458dec7230e469618f3679541c4a1eb3
leiberl <einkabanki02>
2015-01-17 13:05:20 +0100

    dd

--- Case2proj.R
+++ Case2proj.R
@@ -3,5 +3,10 @@ rm(list=ls())
 
 data <- read.table("case2regionsOnePerBatch.txt",header=T)
 
+plot(data$pos[1:52]/data$total[1:52],typ="l")
+data$ratio <- (data$pos/data$total)
 
 
+lm1 <- lm(ratio~.,data)
+anova(lm1)
+lm2 <- step(lm1)
