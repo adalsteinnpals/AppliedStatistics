14d380bd65492caf746b9d3ec7edca5fb7e3f89f
leiberl <einkabanki02>
2015-01-19 14:19:49 +0100

    lkjlkj

--- Case2proj.R
+++ Case2proj.R
@@ -1,75 +1,122 @@
 dev.off() 
 rm(list=ls())
 data <- read.table("case2regionsOnePerBatch.txt",header=T)
-#visualization of the data
+#make new column in the data
 data$ratio <- (data$pos/data$total)
+####################### Visualize the data #######################
 plot(data$ratio,typ="l")
 plot(data$ratio[1:52],typ="l")
 
+#new data.frame that includes only the parameters of interest
 names(data)
-data.primary <- data[,c("aveTemp","relHum","sunHours","precip","ratio")]
+data.primary <- data[,c("aveTemp","maxTemp","relHum","sunHours","precip","ratio")]
+data.primary.vis <- data.primary[!(data.primary$relHum==0),] #delete zeros from the humidity data
+##############!!!!remove the rows that have relHum==0
 
 #plot of the correlation between variables
-pairs(data.primary,panel=panel.smooth)
-<<<<<<< HEAD
+pairs(data.primary.vis,panel=panel.smooth)
+#comment on this plot in the report
+
+#eliminate the na rows 
+data.primary.NAomit0 <- data.primary[complete.cases(data.primary),]
 #comment on plot as it indicates correlation with variables
 ##### 
-#not sure how for implying on the model development
+#use tree for visualizing in which degree the different variables influence the variance
 #install.packages("tree")
 library(tree)
 tree.mod1<-tree(ratio~.,data.primary)
 plot(tree.mod1)
 text(tree.mod1)
-=======
+#the temperature and the sun hours seem to have great impact on the variation (consider these variables)
 
+####################### Analyze the parameters for patterns #######################
+#install.packages("mgcv")
+library(mgcv)
+gam1<-gam(ratio~s(aveTemp)+s(maxTemp)+s(maxTemp)+s(relHum)+s(sunHours)+s(precip),data=data.primary.NAomit0)
+summary(gam1)
+#the s(precip is not significant)
+par(mfrow=c(2,3))
+plot(gam1)
+#Following suggestions from the gam model
+    #aveTemp - pwl or quadratic
+    #maxTemp - linear or quadratic
+    #relHum - linear
+    #sunHours - linear or nonlinear
+    #precip - linear or pwl - not significant though
+####### intermediate test 19.ja ###########
+lm13 <- lm(ratio~aveTemp*maxTemp*relHum*sunHours,data=data.primary.NAomit0)
+summary(lm13)
+lm14 <- step(lm13)
+#drop the highest order term
+drop1(lm14,test="F")
+summary(lm14)
+par(mfrow=c(2,2))
+plot(lm14)
 
-#install.packages("tree")
-library(tree)
-model1<-tree(ratio~.,data.primary)
+for ( i in  c("aveTemp","maxTemp","relHum","sunHours")){
+  plot(data.primary.NAomit0[[i]],residuals(lm14),type="n",xlab=i)
+  panel.smooth(data.primary.NAomit0[[i]],residuals(lm14))
+}
 
-plot(model1)
-text(model1)
->>>>>>> 9444a5f5f61adb6e5732ab54c8b03983bb244840
-#split the aveTemp in 11.25
-#should we use this for our first models???
-#try
 pwl<-function(x,x0){
   return( (x > x0) * (x-x0) )
 }
+optim<-optimize(function(zz){
+  sum( residuals(lm(ratio ~ aveTemp*maxTemp*relHum*sunHours + pwl(aveTemp,zz),data=data.primary.NAomit0))^2 )
+},c(3,8))
+(x0.opt<-optim$minimum)
+#add a pwl for aveTemp
+lm15 <- update(lm14,~.+pwl(aveTemp,x0.opt))
+anova(lm15)
 
-#####
-#try to figure out if higher order term should be added
-#install.packages("mgcv")
-library(mgcv)
-#is the dependency of the parameters on each other the reason for no interactions taking into 
-#consideration?
-gam1<-gam(ratio~s(aveTemp)+s(relHum)+s(sunHours)+s(precip),data=data.primary)
-summary(gam1)
-#all s(<terms>) significant - is that important?
-par(mfrow=c(2,2))
-plot(gam1)
-#yields following considerations:
-#aveTemp - quadratic or linear
-#relHum - linear
-#sunHours - linear, quadratic or nonlinear
-#precip - nonlinear or linear
+lm16 <- step(lm15)
+anova(lm16)
+drop1(lm16,test="F")
+lm17 <- update(lm16,~.-maxTemp:relHum:sunHours)
+anova(lm17)
+plot(lm17)
+for ( i in  c("aveTemp","maxTemp","relHum","sunHours")){
+  plot(data.primary.NAomit0[[i]],residuals(lm17),type="n",xlab=i)
+  panel.smooth(data.primary.NAomit0[[i]],residuals(lm17))
+}
+drop1(lm17,test="F")
+summary(lm17)
 
-#eliminate the na rows - is that right to do?
-data.primary <- data.primary[complete.cases(data.primary),]
-#make a raw lm 
-lm1 <- lm(ratio~aveTemp+relHum+sunHours+precip,data=data.primary)
-summary(lm1)
+#similar as 
+lm18 <- lm(log(ratio+1)~(aveTemp+maxTemp+relHum+sunHours)^2+pwl(aveTemp,x0.opt),data=data.primary.NAomit0)
+summary(lm18)
+plot(lm18)
+
+######
+#make a raw lm with all parameters and interactions
+lm1 <- lm(ratio~aveTemp*maxTemp*relHum*sunHours*precip,data=data.primary.NAomit0)
 anova(lm1)
-#sunHours not significant - is that of interest here?
+drop1(lm1)
+#suggest that no parameters should be eliminated as the highest order term is significant
+#HOW DO WE CHOOSE WHICH PARAMETER INTERACTIONS TO KEEP?
+par(mfrow=c(2,2))
 plot(lm1)
-#clear lack of higher order terms or transformation
+#residuals do not look good as expected
+par(mfrow=c(3,2))
+for ( i in  c("aveTemp","maxTemp","relHum","sunHours","precip")){
+  plot(data.primary.NAomit0[[i]],residuals(lm1),type="n",xlab=i)
+  panel.smooth(data.primary.NAomit0[[i]],residuals(lm1))
+}
+lm12 <- lm(ratio~aveTemp+maxTemp+relHum+sunHours+precip,data.primary.NAomit0)
+par(mfrow=c(2,2))
+plot(lm12)
+#looks better than before
+anova(lm2)
+#sunHours still not significant- but the squared term is
 par(mfrow=c(2,2))
 for ( i in  c("aveTemp","relHum","sunHours","precip")){
-  plot(data.primary[[i]],residuals(lm1),type="n",xlab=i)
-  panel.smooth(data.primary[[i]],residuals(lm1))
+  plot(data.primary[[i]],residuals(lm2),type="n",xlab=i)
+  panel.smooth(data.primary[[i]],residuals(lm2))
 }
-#residuals do not look good as expected try a new model - add square terms
-lm2 <- lm(ratio~aveTemp+relHum+sunHours+precip+I(aveTemp^2)+I(sunHours^2),data.primary)
+
+
+
+lm2 <- lm(ratio~aveTemp+relHum+sunHours+precip+pwl(aveTemp,11.25)+I(aveTemp^2)+I(sunHours^2),data.primary)
 plot(lm2)
 #looks better than before
 anova(lm2)
@@ -80,7 +127,7 @@ for ( i in  c("aveTemp","relHum","sunHours","precip")){
   panel.smooth(data.primary[[i]],residuals(lm2))
 }
 #try third model
-lm3 <- lm(ratio~aveTemp+relHum+sunHours+precip+I(aveTemp^2)+I(sunHours^2)+I(precip^2),data.primary)
+lm3 <- lm(ratio~aveTemp+relHum+sunHours+precip+pwl(aveTemp,11.25)+I(aveTemp^2)+I(sunHours^2)+I(precip^2),data.primary)
 plot(lm3)
 #looks similar as before
 anova(lm3)
@@ -94,7 +141,7 @@ for ( i in  c("aveTemp","relHum","sunHours","precip")){
 #remove the insignificant term which is the same as lm3
 
 #take sqrt of the ratio now
-lm5 <- lm(sqrt(ratio)~aveTemp+relHum+sunHours+precip+I(aveTemp^2)+I(sunHours^2),data.primary)
+lm5 <- lm(sqrt(ratio)~aveTemp+relHum+sunHours+precip+pwl(aveTemp,11.25)+I(aveTemp^2)+I(sunHours^2),data.primary)
 plot(lm5)
 #looks similar as before - makes little difference
 anova(lm5)
