9444a5f5f61adb6e5732ab54c8b03983bb244840
adalsteinnpals <adalsteinnpals@gmail.com>
2015-01-19 10:02:15 +0100

    a

--- Case2proj.R
+++ Case2proj.R
@@ -15,8 +15,9 @@ data.primary <- data[,c("aveTemp","relHum","sunHours","precip","ratio")]
 #plot of the correlation between variables
 pairs(data.primary,panel=panel.smooth)
 
+
 #install.packages("tree")
-#library(tree)
+library(tree)
 model1<-tree(ratio~.,data.primary)
 
 plot(model1)
@@ -33,7 +34,27 @@ plot(model2)
 
 
 
+
 lm1 <- lm(ratio~.,data.primary)
 
 anova(lm1)
 lm2 <- step(lm1)
+
+
+
+
+
+
+#### Part2 - Analyzis of position
+
+data$ratioR1 = (data$R1pos/data$R1total)
+data$ratioR2 = (data$R2pos/data$R2total)
+data$ratioR3 = (data$R3pos/data$R3total)
+data$ratioR4 = (data$R4pos/data$R4total)
+data$ratioR5 = (data$R5pos/data$R5total)
+data$ratioR6 = (data$R6pos/data$R6total)
+data$ratioR7 = (data$R7pos/data$R7total)
+data$ratioR8 = (data$R8pos/data$R8total)
+
+
+
